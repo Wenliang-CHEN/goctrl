ffa960283f3a7ef5bfb0db8c17603a7ec299d581
Bruce C.W.L <bruceadmin@Bruces-MBP.fritz.box>
2018-06-03 11:19:35 +0200

    Add function to run command in os and print to stdout

--- goctrl.go
+++ goctrl.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
-	"os.exec"
+	"os/exec"
 )
 
 const errInvalidCommand = "Invalid command"
@@ -24,10 +26,38 @@ func main() {
 
 func startMinikube() {
 	fmt.Println("starting minikube")
+	runOsCommand("minikube", "start")
 }
 
 func stopMinikube() {
 	fmt.Println("stoping minikube")
+}
+
+func runOsCommand(rawCmd string, args ...string) {
+	var stdoutBuf bytes.Buffer
+	cmd := exec.Command(rawCmd, args...)
+
+	stdoutIn, _ := cmd.StdoutPipe()
+	stdout := io.MultiWriter(os.Stdout, &stdoutBuf)
+	err := cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	var errStdout error
+	go func() {
+		_, errStdout = io.Copy(stdout, stdoutIn)
+	}()
+
+	err = cmd.Wait()
+	if err != nil {
+		panic(err)
+	}
+	if errStdout != nil {
+		panic("failed to capture stdout")
+	}
+	outStr := string(stdoutBuf.Bytes())
+	fmt.Printf("\nout:\n%s", outStr)
 }
 
 func handleError() {
@@ -41,11 +71,11 @@ func handleError() {
 		fmt.Println(err)
 		printHelpText()
 	default:
-		fmt.Println("Unhandled Error")
+		fmt.Println(err)
 	}
 }
 
-# TODO
+// TODO
 func printHelpText() {
 	fmt.Println("this is help text")
 }
